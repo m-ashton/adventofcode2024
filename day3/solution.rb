67ad593bdd9ac64a469585e5f93e106076141f00
Mark Ashton <meashton@gmail.com>
2025-03-19 17:45:29 -0500

    Day 3 Part 2

--- day3/solution.rb
+++ day3/solution.rb
@@ -16,13 +16,40 @@ def part1
     start_pos = /mul\((\d{1,3}),(\d{1,3})\)/ =~ segment
     break unless start_pos
     total += $1.to_i * $2.to_i
-    segment = segment[start_pos + 6 + $1.length + $2.length..-1]
+    segment = segment[start_pos + "mul(#{1},#{2})".length..-1]
   end
 
   total
 end
 
 def part2
+  program = get_input('./input.txt')
+  segment = program
+  start_pos = 0
+  total = 0
+  enabled = true
+  loop do
+    next_mul = /mul\((\d{1,3}),(\d{1,3})\)/ =~ segment
+    first, second = [$1, $2]
+    break unless next_mul
+    next_do = /do\(\)/ =~ segment
+    next_dont = /don't\(\)/ =~ segment
+    if !enabled && next_do && next_do < next_mul
+      enabled = true
+      start_pos = next_do + "do()".length
+    elsif enabled && next_dont && next_dont < next_mul
+      enabled = false
+      start_pos = next_mul + "mul(#{first},#{second})".length
+    elsif enabled
+      total += first.to_i * second.to_i
+      start_pos = next_mul + "mul(#{first},#{second})".length
+    else
+      start_pos = next_mul + "mul(#{first},#{second})".length
+    end
+    segment = segment[start_pos..-1]
+  end
+
+  total
 end
 
 ARGV[0] == '2' ? puts(part2) : puts(part1)

c4f92c37b28c6838f6566fb965ae21f2ef065ac7
Mark Ashton <meashton@gmail.com>
2025-07-27 20:47:35 -0500

    day 8 part 1

---
+++ day8/solution.rb
@@ -0,0 +1,40 @@
+def compute_antinodes(position_a, position_b)
+  delta = [position_a[0] - position_b[0], position_a[1] - position_b[1]]
+  [
+    [position_a[0] + delta[0], position_a[1] + delta[1]],
+    [position_b[0] - delta[0], position_b[1] - delta[1]]
+  ]
+end
+
+def read_input(filename)
+  grid = Hash.new { |h, k| h[k] = Array.new }
+  lines = File.open(filename).readlines.map(&:strip)
+  lines.each.with_index do |line, y|
+    line.strip.chars.each.with_index do |c, x|
+      grid[c] << [x, y] if c != '.'
+    end
+  end
+  width = lines.first.chars.count
+  height = lines.count
+  [grid, width, height]
+end
+
+def part1
+  grid, width, height = read_input('./input.txt')
+  antinodes = Set.new
+  grid.each do |_, positions|
+    positions.combination(2) do |antenna_pair|
+      candidate_antinodes = compute_antinodes(*antenna_pair)
+      inbounds_antinodes = candidate_antinodes.filter do |antinode|
+        (0...width).include?(antinode[0]) && (0...height).include?(antinode[1])
+      end
+      antinodes.merge(inbounds_antinodes)
+    end
+  end
+  puts antinodes.count
+end
+
+def part2
+end
+
+ARGV[0] == '2' ? puts(part2) : puts(part1)

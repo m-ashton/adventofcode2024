a97740bc5affd43fef52321cc6e3999b38d88848
Mark Ashton <meashton@gmail.com>
2025-09-10 16:30:51 -0500

    working commit for day 9 part 2

--- day9/solution.rb
+++ day9/solution.rb
@@ -36,40 +36,39 @@ def initialize_disk_part_2(filename)
       disk_pos += value
     end
   end
-
+  
   [disk, metadata]
 end
 
 def move_file(disk, metadata, id:, to:)
   return unless id && to
-
+  
   file_metadata = metadata[:files][id]
-
+  
   (to...to + file_metadata[:size]).each do |pos|
     disk[pos] = id
   end
-
+  
   (file_metadata[:start]...file_metadata[:start] + file_metadata[:size]).each do |pos|
     disk[pos] = nil
   end
-
+  
   file_metadata[:start] = to
   file_metadata[:compacted] = true
-
+  
   space_metadata = metadata[:free_space].delete(to)
   if file_metadata[:size] < space_metadata[:size]
     metadata[:free_space][to + file_metadata[:size]] = {size: space_metadata[:size] - file_metadata[:size]}
     return to + file_metadata[:size]
   end
-  puts metadata.inspect
 end
 
 def part1
   disk = initialize_disk('./input.txt')
   last_file_block_pos = disk.length - 1
   last_file_block_pos -= 1 while disk[last_file_block_pos].nil?
-
-  (0...disk.size).each do |i|
+  
+  (1...disk.size).each do |i|
     break if i == last_file_block_pos
     if disk[i].nil?
       disk[i] = disk[last_file_block_pos]
@@ -81,18 +80,18 @@ def part1
 end
 
 def part2
-  disk, metadata = initialize_disk_part_2('./sample.txt')
-
-  puts metadata.inspect
-  metadata[:free_space].keys.sort.each do |position|
-    # FIXME infinite loop once we're done
-    until position.nil?
-      metadata[:files].reject { |k, v| v[:compacted] }.keys.sort.reverse.each do |file_id|
-        if metadata[:files][file_id][:size] <= metadata[:free_space][position][:size]
-          position = move_file(disk, metadata, id: file_id, to: position)
-          puts disk.inspect
-          break
-        end
+  disk, metadata = initialize_disk_part_2('./input.txt')
+  
+  metadata[:files].reject { |k, v| v[:compacted] }.keys.sort.reverse.each do |file_id|
+    # puts '---------'
+    # puts disk.inspect
+    # puts metadata.inspect
+    # puts '---------'
+    free_positions = metadata[:free_space].keys.sort
+    free_positions.each do |position|
+      if metadata[:files][file_id][:size] <= metadata[:free_space][position][:size]
+        move_file(disk, metadata, id: file_id, to: position)
+        break
       end
     end
   end

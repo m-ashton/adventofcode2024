8e6f3d6ac4bc2d4ce0ca16fb4a21c688ef4adce3
Mark Ashton <meashton@gmail.com>
2025-09-24 07:27:50 -0500

    Day 11 part 2

--- day11/solution.rb
+++ day11/solution.rb
@@ -1,31 +1,48 @@
 def read_file(path)
-  File.read(path).split.map(&:to_i)
+  Hash.new { |h, k| h[k] = 0 }.tap do |stone_counts|
+    File.read(path).split.map(&:to_i).each do |stone|
+      stone_counts[stone] += 1
+    end
+  end
 end
 
-def blink(stones)
-    stones.flat_map do |stone|
-      if stone == 0
-        1
-      elsif stone.digits.count.even?
-        num_digits = stone.digits.count / 2
-        # FIXME: why are the digits reversed? Is there a cleaner way than join.to_i ?
-        [stone.digits.reverse.first(num_digits).join.to_i,
-        stone.digits.reverse.last(num_digits).join.to_i]
-      else
-        stone * 2024
+def blink(stone)
+  if stone == 0
+    [1]
+  elsif stone.digits.count.even?
+    num_digits = stone.digits.count / 2
+    [stone.digits.last(num_digits).map.with_index { |x, i| x * 10 ** i }.sum,
+    stone.digits.first(num_digits).map.with_index { |x, i| x * 10 ** i }.sum]
+  else
+    [stone * 2024]
+  end
+end
+
+def new_counts(stone_counts)
+  Hash.new { |h, k| h[k] = 0 }.tap do |new_stone_counts|
+    stone_counts.each do |stone, count|
+      new_stones = blink(stone)
+      new_stones.each do |new_stone|
+        new_stone_counts[new_stone] += stone_counts[stone]
       end
     end
+  end
 end
 
 def part1
-  stones = read_file('./input.txt')
+  stone_counts = read_file('./input.txt')
   25.times do
-    stones = blink(stones)
+    stone_counts = new_counts(stone_counts)
   end
-  stones.count
+  stone_counts.values.sum
 end
 
 def part2
+  stone_counts = read_file('./input.txt')
+  75.times do |i|
+    stone_counts = new_counts(stone_counts)
+  end
+  stone_counts.values.sum
 end
 
 ARGV[0] == '2' ? puts(part2) : puts(part1)
